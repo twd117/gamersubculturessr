259df0136850e5dd423a47d14d2322da3d3b2963
neptunestudio <78939974+neptunestudi0@users.noreply.github.com>
2022-11-17 02:51:02 +0100

    ads space4

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -21,7 +21,12 @@
        
        
        
-
+         <ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-1291356316800764"
+     data-ad-slot="5853275170"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
    
    
         
@@ -97,8 +102,12 @@ export default defineComponent({
     adScript.setAttribute("src", "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1291356316800764");
     adScript.setAttribute("crossorigin","anonymous");
 
-    document.head.appendChild(adScript);
-   
+    document.body.appendChild(adScript);
+    let adpushScript = document.createElement("script");
+    adpushScript.innerHTML="(adsbygoogle = window.adsbygoogle || []).push({});"
+
+    document.body.appendChild(adpushScript);
+
     
      
   
